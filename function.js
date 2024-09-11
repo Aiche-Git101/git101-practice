8ea8f7b9f08af14ae50a7544f5a2bf79b7460088
Henrikki <henrikki.gren@hotmail.com>
2024-09-11 16:35:34 +0300

    added a function that only pushes to array if value doesnt exist already

--- function.js
+++ function.js
@@ -156,3 +156,13 @@ const prepend = (value, array)=>{
  * @return {string} 
  */
 const formatBytes = (a,b) =>{if(0==a)return"0 Bytes";let c=1024,d=b||2,e=["Bytes","KB","MB","GB","TB","PB","EB","ZB","YB"],f=Math.floor(Math.log(a)/Math.log(c));return parseFloat((a/Math.pow(c,f)).toFixed(d))+" "+e[f]};
+
+/** only push to array if value doesn't exist already
+ * @param {array} array
+ * @param {element} element
+ */
+const array_push = (arr,el)=>{
+    if(!arr.includes(el)){
+        arr.push(el);
+    }
+}
